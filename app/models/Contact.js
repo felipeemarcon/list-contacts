574ddfc9fa973cd897e07c80f70cce52069c2033
Felipe Marcon <felipeemarcon@gmail.com>
2019-11-10 21:15:43 -0300

    feat: add avatar upload to contact

--- app/models/Contact.js
+++ app/models/Contact.js
@@ -1,25 +1,34 @@
 const mongoose = require("mongoose");
 
-const ContactSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: true
+const ContactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true
+    },
+    cel_phone: {
+      type: String,
+      require: false
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    address: {
+      type: String
+    },
+    avatar: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Avatar"
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
   },
-  cel_phone: {
-    type: String,
-    require: false
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  address: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
+  {
+    timestamps: true
   }
-});
+);
 
 module.exports = mongoose.model("Contact", ContactSchema);
