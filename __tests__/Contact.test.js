195f5b741ec0d82bfa8bea2dd6208151f543e8e0
Felipe Marcon <felipe.marcon@gamersclub.com.br>
2019-11-13 15:00:32 -0300

    test: add test to delete a contact and create env.test

--- __tests__/Contact.test.js
+++ __tests__/Contact.test.js
@@ -1,20 +1,42 @@
 const request = require("supertest");
 const app = require("../app/app");
-// const Contact = require("../app/models/Contact");
+
+const Contact = require("../app/models/Contact");
+
+const contactId = "5dcc1d6b7485e352c25b2e75"
 
 describe("Contact", () => {
-  it("should create a contact", async done => {
+  it("should create a contact", async (done) => {
+    const contact = await Contact.create({
+      name: "Matheus Santos",
+      cel_phone: "15997958677",
+      email: "matheus@gmail.com",
+      address: "R. Emerenciano Prestes de Barros, 680 - Sorocaba/SP - Brasil"
+    })
+
     const response = await request(app)
       .post("/contact")
       .send({
-        name: "Matheus Santos",
-        cel_phone: "15997958677",
-        email: "matheus@gmail.com",
-        address: "R. Emerenciano Prestes de Barros, 680 - Sorocaba/SP - Brasil"
+        _id: contactId,
+        name: contact.name,
+        cel_phone: contact.cel_phone,
+        email: contact.email,
+        address: contact.address
       });
 
     expect(response.status).toBe(200);
 
     done();
+
   });
+
+  it("should remove a contact", async (done) => {
+    const response = await request(app)
+      .del(`/contact/${contactId}`);
+
+      expect(response.status).toBe(200);
+
+      done();
+
+  })
 });
