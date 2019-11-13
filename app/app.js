2234f607ddc37ee559181d8973a86cf1d18eb644
Felipe Marcon <felipeemarcon@gmail.com>
2019-11-13 10:38:32 -0300

    refactor: refactor database call

--- app/app.js
+++ app/app.js
@@ -1,12 +1,20 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
+const { connectDb } = require("./config/database");
 
 class AppController {
   constructor() {
     this.express = express();
 
+    this.database();
     this.middlewares();
     this.routes();
+  }
+
+  database() {
+    connectDb();
   }
 
   middlewares() {
--- app/config/database.js
+++ app/config/database.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
 const uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
